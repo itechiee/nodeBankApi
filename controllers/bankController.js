bfe3aab36aa7bbf1f3e9cc7e82989638d7b4f52c
Prabu <prabu.itech@gmail.com>
2019-04-05 14:42:27 +0800

    webhooks

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -1,6 +1,5 @@
 var db=require('../db');
-var Bank = require('../models/Bank');
-
+var request = require('request');
 
 exports.Payment = function(req, res, next) {
   let txnId = Math.random().toString(36).substring(2);
@@ -29,8 +28,12 @@ exports.Payment = function(req, res, next) {
               TXN_DATE: new Date().toISOString(),
               RESPONSE_CODE_DESCRIPTION: 'Approved'
           }    
-
-
-          return res.status(200).json({ status: 200, error: null, response: successMsg });
+          request.post({url:'http://localhost/checkout/callback', formData: successMsg}, function optionalCallback(err, httpResponse, body) {
+            if (err) {
+              return console.error('upload failed:', err);
+            }
+            return res.status(200).json({ status: 200, error: null, response: successMsg });
+          });
+          
     });
 };
